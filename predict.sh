9599dd89d609fdf1f732a6df0f1efa3aecb4492f
goyalnishant <42680289+goyalnishant@users.noreply.github.com>
2019-11-11 14:57:17 +0530

    update filepath to use trained model

--- predict.sh
+++ predict.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 if [ $1 == "ngram" ] ;
 then
-python ngram.py $2 $3 "${@:4}"
+python ngram.py $2 $3 $4 "${@:5}"
 
 elif [ $1 == "lstm" ];
 then
-python lstm.py $2 $3 "${@:4}"
+python lstm.py $2 $3 $4 "${@:5}"
 fi;
