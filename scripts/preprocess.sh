b3c5c5a4cad861d76d3f671a68f452b0b952cc11
priyansh2 <priyanshagarwal02may@gmail.com>
2019-11-12 01:18:30 +0530

    test commit

--- scripts/preprocess.sh
+++ scripts/preprocess.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 #SBATCH -A research
-#SBATCH -n 15
+#SBATCH -n 10
 #SBATCH --qos=medium
 #SBATCH -p long
 #SBATCH --gres=gpu:0
 #SBATCH --mem-per-cpu=3G
 #SBATCH --time=4-00:00:00
 source /home/$USER/p3.6/bin/activate
-python /home/priyansh.agrawal/ire_final_project/scripts/preprocess.py
+module load cuda/9.0
+module load cudnn/7-cuda-9.0
+python /home/priyansh.agrawal/ire_final_project/scripts/lstm.py
