cb311f4968dede5a21c4305a6a08c25a4e939f90
goyalnishant <42680289+goyalnishant@users.noreply.github.com>
2019-11-11 19:25:27 +0530

    learning rate and batch size added

--- train.sh
+++ train.sh
@@ -5,6 +5,6 @@ python ngram.py -g 0.5 -d 0.75 $2 $3
 
 elif [ $1 == "lstm" ];
 then
-python lstm.py -h 1 -n 500 -d 0.1 -e 100 $2 $3
+python lstm.py -h 1 -n 500 -d 0.1 -e 100 -l 0.001 -b 128 $2 $3
 fi;
 
